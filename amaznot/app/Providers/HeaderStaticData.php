bf642232547cbdeced2a854184d68bb44d378f9f
Ghaith Chrit <ghaithfinadnan@gmail.com>
2022-02-12 19:39:55 -0500

    #53-resolved-conflicts

---
+++ amaznot/app/Providers/HeaderStaticData.php
@@ -0,0 +1,410 @@
+<?php
+
+namespace App\Providers;
+
+class HeaderStaticData
+{
+    public static $categories = array(
+        0 =>
+        array(
+            'category' => 'Toys and Games',
+            'subcategory' => 'Dolls and Accessories',
+        ),
+        1 =>
+        array(
+            'category' => 'Toys and Games',
+            'subcategory' => 'Kids\' Electronics',
+        ),
+        2 =>
+        array(
+            'category' => 'Toys and Games',
+            'subcategory' => 'Building Toys',
+        ),
+        3 =>
+        array(
+            'category' => 'Toys and Games',
+            'subcategory' => 'Sports and Outdoor Play',
+        ),
+        4 =>
+        array(
+            'category' => 'Toys and Games',
+            'subcategory' => 'Stuffed Animals and Plush Toys',
+        ),
+        5 =>
+        array(
+            'category' => 'Toys and Games',
+            'subcategory' => 'Play Vehicles',
+        ),
+        6 =>
+        array(
+            'category' => 'Toys and Games',
+            'subcategory' => 'Collectible Toys',
+        ),
+        7 =>
+        array(
+            'category' => 'Toys and Games',
+            'subcategory' => 'Games and Accessories',
+        ),
+        8 =>
+        array(
+            'category' => 'Toys and Games',
+            'subcategory' => 'Puzzles',
+        ),
+        9 =>
+        array(
+            'category' => 'Toys and Games',
+            'subcategory' => 'Hobbies',
+        ),
+        10 =>
+        array(
+            'category' => 'Toys and Games',
+            'subcategory' => 'Dress Up and Pretend Play',
+        ),
+        11 =>
+        array(
+            'category' => 'Toys and Games',
+            'subcategory' => 'Arts and Crafts',
+        ),
+        12 =>
+        array(
+            'category' => 'Toys and Games',
+            'subcategory' => 'Novelty and Gag Toys',
+        ),
+        13 =>
+        array(
+            'category' => 'Toys and Games',
+            'subcategory' => 'Learning and Education',
+        ),
+        14 =>
+        array(
+            'category' => 'Toys and Games',
+            'subcategory' => 'Puppets and Puppet Theaters',
+        ),
+        15 =>
+        array(
+            'category' => 'Toys and Games',
+            'subcategory' => 'Party Supplies',
+        ),
+        16 =>
+        array(
+            'category' => 'Toys and Games',
+            'subcategory' => 'Baby and Toddler Toys',
+        ),
+        17 =>
+        array(
+            'category' => 'Tools and Home Improvement',
+            'subcategory' => 'Paint, Wall Treatments and Supplies',
+        ),
+        18 =>
+        array(
+            'category' => 'Tools and Home Improvement',
+            'subcategory' => 'Lighting and Ceiling Fans',
+        ),
+        19 =>
+        array(
+            'category' => 'Tools and Home Improvement',
+            'subcategory' => 'Electrical',
+        ),
+        20 =>
+        array(
+            'category' => 'Tools and Home Improvement',
+            'subcategory' => 'Hardware',
+        ),
+        21 =>
+        array(
+            'category' => 'Tools and Home Improvement',
+            'subcategory' => 'Power and Hand Tools',
+        ),
+        22 =>
+        array(
+            'category' => 'Sports and Outdoors',
+            'subcategory' => 'Fan Shop',
+        ),
+        23 =>
+        array(
+            'category' => 'Sports and Outdoors',
+            'subcategory' => 'Sports and Fitness',
+        ),
+        24 =>
+        array(
+            'category' => 'Sports and Outdoors',
+            'subcategory' => 'Outdoor Recreation',
+        ),
+        25 =>
+        array(
+            'category' => 'Remote and App Controlled Vehicles and Parts',
+            'subcategory' => 'Remote and App Controlled Vehicle Parts',
+        ),
+        26 =>
+        array(
+            'category' => 'Remote and App Controlled Vehicle Parts',
+            'subcategory' => 'Power Plant and Driveline Systems',
+        ),
+        27 =>
+        array(
+            'category' => 'Pet Supplies',
+            'subcategory' => 'Dogs',
+        ),
+        28 =>
+        array(
+            'category' => 'Pet Supplies',
+            'subcategory' => 'Top Selection from AmazonPets',
+        ),
+        29 =>
+        array(
+            'category' => 'Patio, Lawn and Garden',
+            'subcategory' => 'Patio Furniture and Accessories',
+        ),
+        30 =>
+        array(
+            'category' => 'Patio, Lawn and Garden',
+            'subcategory' => 'Gardening and Lawn Care',
+        ),
+        31 =>
+        array(
+            'category' => 'Patio, Lawn and Garden',
+            'subcategory' => 'Pools, Hot Tubs and Supplies',
+        ),
+        32 =>
+        array(
+            'category' => 'Office Products',
+            'subcategory' => 'Office Electronics',
+        ),
+        33 =>
+        array(
+            'category' => 'Office Products',
+            'subcategory' => 'Office and School Supplies',
+        ),
+        34 =>
+        array(
+            'category' => 'Industrial and Scientific',
+            'subcategory' => 'Power Transmission Products',
+        ),
+        35 =>
+        array(
+            'category' => 'Industrial and Scientific',
+            'subcategory' => 'Science Education',
+        ),
+        36 =>
+        array(
+            'category' => 'Industrial and Scientific',
+            'subcategory' => 'Lab and Scientific Products',
+        ),
+        37 =>
+        array(
+            'category' => 'Industrial and Scientific',
+            'subcategory' => 'Professional Medical Supplies',
+        ),
+        38 =>
+        array(
+            'category' => 'Industrial and Scientific',
+            'subcategory' => 'Industrial Electrical',
+        ),
+        39 =>
+        array(
+            'category' => 'Industrial and Scientific',
+            'subcategory' => 'Fasteners',
+        ),
+        40 =>
+        array(
+            'category' => 'Home and Kitchen',
+            'subcategory' => 'Furniture',
+        ),
+        41 =>
+        array(
+            'category' => 'Home and Kitchen',
+            'subcategory' => 'Bedding',
+        ),
+        42 =>
+        array(
+            'category' => 'Home and Kitchen',
+            'subcategory' => 'Kitchen and Dining',
+        ),
+        43 =>
+        array(
+            'category' => 'Home and Kitchen',
+            'subcategory' => 'Wall Art',
+        ),
+        44 =>
+        array(
+            'category' => 'Home and Kitchen',
+            'subcategory' => 'Event and Party Supplies',
+        ),
+        45 =>
+        array(
+            'category' => 'Home and Kitchen',
+            'subcategory' => 'Storage and Organization',
+        ),
+        46 =>
+        array(
+            'category' => 'Hobbies',
+            'subcategory' => 'Remote and App Controlled Vehicles and Parts',
+        ),
+        47 =>
+        array(
+            'category' => 'Health and Household',
+            'subcategory' => 'Household Supplies',
+        ),
+        48 =>
+        array(
+            'category' => 'Health and Household',
+            'subcategory' => 'Medical Supplies and Equipment',
+        ),
+        49 =>
+        array(
+            'category' => 'Health and Household',
+            'subcategory' => 'Stationery and Gift Wrapping Supplies',
+        ),
+        50 =>
+        array(
+            'category' => 'Grocery and Gourmet Food',
+            'subcategory' => 'Pantry Staples',
+        ),
+        51 =>
+        array(
+            'category' => 'Clothing, Shoes and Jewelry',
+            'subcategory' => 'Shoe, Jewelry and Watch Accessories',
+        ),
+        52 =>
+        array(
+            'category' => 'Clothing, Shoes and Jewelry',
+            'subcategory' => 'Women',
+        ),
+        53 =>
+        array(
+            'category' => 'Clothing, Shoes and Jewelry',
+            'subcategory' => 'Girls',
+        ),
+        54 =>
+        array(
+            'category' => 'Clothing, Shoes and Jewelry',
+            'subcategory' => 'Luggage and Travel Gear',
+        ),
+        55 =>
+        array(
+            'category' => 'Clothing, Shoes and Jewelry',
+            'subcategory' => 'Men',
+        ),
+        56 =>
+        array(
+            'category' => 'Clothing, Shoes and Jewelry',
+            'subcategory' => 'Novelty and More',
+        ),
+        57 =>
+        array(
+            'category' => 'Clothing, Shoes and Jewelry',
+            'subcategory' => 'Boys',
+        ),
+        58 =>
+        array(
+            'category' => 'Clothing, Shoes and Jewelry',
+            'subcategory' => 'Costumes and Accessories',
+        ),
+        59 =>
+        array(
+            'category' => 'Beauty and Personal Care',
+            'subcategory' => 'Foot, Hand and Nail Care',
+        ),
+        60 =>
+        array(
+            'category' => 'Beauty and Personal Care',
+            'subcategory' => 'Tools and Accessories',
+        ),
+        61 =>
+        array(
+            'category' => 'Beauty and Personal Care',
+            'subcategory' => 'Makeup',
+        ),
+        62 =>
+        array(
+            'category' => 'Beauty and Personal Care',
+            'subcategory' => 'Skin Care',
+        ),
+        63 =>
+        array(
+            'category' => 'Beauty and Personal Care',
+            'subcategory' => 'Hair Care',
+        ),
+        64 =>
+        array(
+            'category' => 'Beauty and Personal Care',
+            'subcategory' => 'Personal Care',
+        ),
+        65 =>
+        array(
+            'category' => 'Baby Products',
+            'subcategory' => 'Gifts',
+        ),
+        66 =>
+        array(
+            'category' => 'Baby Products',
+            'subcategory' => 'Strollers and Accessories',
+        ),
+        67 =>
+        array(
+            'category' => 'Baby Products',
+            'subcategory' => 'Nursery',
+        ),
+        68 =>
+        array(
+            'category' => 'Baby Products',
+            'subcategory' => 'Feeding',
+        ),
+        69 =>
+        array(
+            'category' => 'Baby Products',
+            'subcategory' => 'Potty Training',
+        ),
+        70 =>
+        array(
+            'category' => 'Baby Products',
+            'subcategory' => 'Baby Care',
+        ),
+        71 =>
+        array(
+            'category' => 'Baby Products',
+            'subcategory' => 'Activity and Entertainment',
+        ),
+        72 =>
+        array(
+            'category' => 'Arts, Crafts and Sewing',
+            'subcategory' => 'Printmaking',
+        ),
+        73 =>
+        array(
+            'category' => 'Arts, Crafts and Sewing',
+            'subcategory' => 'Painting, Drawing and Art Supplies',
+        ),
+        74 =>
+        array(
+            'category' => 'Arts, Crafts and Sewing',
+            'subcategory' => 'Sewing',
+        ),
+        75 =>
+        array(
+            'category' => 'Arts, Crafts and Sewing',
+            'subcategory' => 'Fabric',
+        ),
+        76 =>
+        array(
+            'category' => 'Arts, Crafts and Sewing',
+            'subcategory' => 'Crafting',
+        ),
+        77 =>
+        array(
+            'category' => 'Arts, Crafts and Sewing',
+            'subcategory' => 'Beading and Jewelry Making',
+        ),
+        78 =>
+        array(
+            'category' => 'Arts, Crafts and Sewing',
+            'subcategory' => 'Needlework',
+        ),
+        79 =>
+        array(
+            'category' => 'Arts, Crafts and Sewing',
+            'subcategory' => 'Scrapbooking and Stamping',
+        ),
+    );
+    public static $categoriesString = '[{"category":"Toys and Games","subcategory":"Dolls and Accessories"},{"category":"Toys and Games","subcategory":"Kids\' Electronics"},{"category":"Toys and Games","subcategory":"Building Toys"},{"category":"Toys and Games","subcategory":"Sports and Outdoor Play"},{"category":"Toys and Games","subcategory":"Stuffed Animals and Plush Toys"},{"category":"Toys and Games","subcategory":"Play Vehicles"},{"category":"Toys and Games","subcategory":"Collectible Toys"},{"category":"Toys and Games","subcategory":"Games and Accessories"},{"category":"Toys and Games","subcategory":"Puzzles"},{"category":"Toys and Games","subcategory":"Hobbies"},{"category":"Toys and Games","subcategory":"Dress Up and Pretend Play"},{"category":"Toys and Games","subcategory":"Arts and Crafts"},{"category":"Toys and Games","subcategory":"Novelty and Gag Toys"},{"category":"Toys and Games","subcategory":"Learning and Education"},{"category":"Toys and Games","subcategory":"Puppets and Puppet Theaters"},{"category":"Toys and Games","subcategory":"Party Supplies"},{"category":"Toys and Games","subcategory":"Baby and Toddler Toys"},{"category":"Tools and Home Improvement","subcategory":"Paint, Wall Treatments and Supplies"},{"category":"Tools and Home Improvement","subcategory":"Lighting and Ceiling Fans"},{"category":"Tools and Home Improvement","subcategory":"Electrical"},{"category":"Tools and Home Improvement","subcategory":"Hardware"},{"category":"Tools and Home Improvement","subcategory":"Power and Hand Tools"},{"category":"Sports and Outdoors","subcategory":"Fan Shop"},{"category":"Sports and Outdoors","subcategory":"Sports and Fitness"},{"category":"Sports and Outdoors","subcategory":"Outdoor Recreation"},{"category":"Remote and App Controlled Vehicles and Parts","subcategory":"Remote and App Controlled Vehicle Parts"},{"category":"Remote and App Controlled Vehicle Parts","subcategory":"Power Plant and Driveline Systems"},{"category":"Pet Supplies","subcategory":"Dogs"},{"category":"Pet Supplies","subcategory":"Top Selection from AmazonPets"},{"category":"Patio, Lawn and Garden","subcategory":"Patio Furniture and Accessories"},{"category":"Patio, Lawn and Garden","subcategory":"Gardening and Lawn Care"},{"category":"Patio, Lawn and Garden","subcategory":"Pools, Hot Tubs and Supplies"},{"category":"Office Products","subcategory":"Office Electronics"},{"category":"Office Products","subcategory":"Office and School Supplies"},{"category":"Industrial and Scientific","subcategory":"Power Transmission Products"},{"category":"Industrial and Scientific","subcategory":"Science Education"},{"category":"Industrial and Scientific","subcategory":"Lab and Scientific Products"},{"category":"Industrial and Scientific","subcategory":"Professional Medical Supplies"},{"category":"Industrial and Scientific","subcategory":"Industrial Electrical"},{"category":"Industrial and Scientific","subcategory":"Fasteners"},{"category":"Home and Kitchen","subcategory":"Furniture"},{"category":"Home and Kitchen","subcategory":"Bedding"},{"category":"Home and Kitchen","subcategory":"Kitchen and Dining"},{"category":"Home and Kitchen","subcategory":"Wall Art"},{"category":"Home and Kitchen","subcategory":"Event and Party Supplies"},{"category":"Home and Kitchen","subcategory":"Storage and Organization"},{"category":"Hobbies","subcategory":"Remote and App Controlled Vehicles and Parts"},{"category":"Health and Household","subcategory":"Household Supplies"},{"category":"Health and Household","subcategory":"Medical Supplies and Equipment"},{"category":"Health and Household","subcategory":"Stationery and Gift Wrapping Supplies"},{"category":"Grocery and Gourmet Food","subcategory":"Pantry Staples"},{"category":"Clothing, Shoes and Jewelry","subcategory":"Shoe, Jewelry and Watch Accessories"},{"category":"Clothing, Shoes and Jewelry","subcategory":"Women"},{"category":"Clothing, Shoes and Jewelry","subcategory":"Girls"},{"category":"Clothing, Shoes and Jewelry","subcategory":"Luggage and Travel Gear"},{"category":"Clothing, Shoes and Jewelry","subcategory":"Men"},{"category":"Clothing, Shoes and Jewelry","subcategory":"Novelty and More"},{"category":"Clothing, Shoes and Jewelry","subcategory":"Boys"},{"category":"Clothing, Shoes and Jewelry","subcategory":"Costumes and Accessories"},{"category":"Beauty and Personal Care","subcategory":"Foot, Hand and Nail Care"},{"category":"Beauty and Personal Care","subcategory":"Tools and Accessories"},{"category":"Beauty and Personal Care","subcategory":"Makeup"},{"category":"Beauty and Personal Care","subcategory":"Skin Care"},{"category":"Beauty and Personal Care","subcategory":"Hair Care"},{"category":"Beauty and Personal Care","subcategory":"Personal Care"},{"category":"Baby Products","subcategory":"Gifts"},{"category":"Baby Products","subcategory":"Strollers and Accessories"},{"category":"Baby Products","subcategory":"Nursery"},{"category":"Baby Products","subcategory":"Feeding"},{"category":"Baby Products","subcategory":"Potty Training"},{"category":"Baby Products","subcategory":"Baby Care"},{"category":"Baby Products","subcategory":"Activity and Entertainment"},{"category":"Arts, Crafts and Sewing","subcategory":"Printmaking"},{"category":"Arts, Crafts and Sewing","subcategory":"Painting, Drawing and Art Supplies"},{"category":"Arts, Crafts and Sewing","subcategory":"Sewing"},{"category":"Arts, Crafts and Sewing","subcategory":"Fabric"},{"category":"Arts, Crafts and Sewing","subcategory":"Crafting"},{"category":"Arts, Crafts and Sewing","subcategory":"Beading and Jewelry Making"},{"category":"Arts, Crafts and Sewing","subcategory":"Needlework"},{"category":"Arts, Crafts and Sewing","subcategory":"Scrapbooking and Stamping"}]';
+}
