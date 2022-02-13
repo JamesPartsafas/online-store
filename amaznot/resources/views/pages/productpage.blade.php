63d0593072971521011b2338634408ce7cf0ee35
Christina Darstbanian <minasderstep@gmail.com>
2022-02-13 14:52:09 -0500

    #54 fixed some bugs in the product pages related to add button

--- amaznot/resources/views/pages/productpage.blade.php
+++ amaznot/resources/views/pages/productpage.blade.php
@@ -22,21 +22,7 @@
     <?php echo "Price:"; ?> {{  $product['price'] }} <?php echo "$"; ?>
 
      <?php echo nl2br("\n"); ?>   <?php echo "Weight:"; ?>{{ $product['weight'] }}
-     <form  method='post'>
-<div class="btn-group" role="group" aria-label="Basic mixed styles example">
-<button type='button'  style="border-radius: 4px;"class="btn btn-primary" onclick='modify(this,false)'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash-square" viewBox="0 0 16 16"><path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
-  <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z"/></svg>
-  </button>
- 
-<input type='text'  id="result" value="1"  size='1' style="text-align:center;border-radius: 4px;" disabled >
-<button style="margin-right:14px;border-radius: 4px;" type='button' class="btn btn-primary" onclick='modify(this,true)' ><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16"><path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
-  <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/></svg>
-  </button>
-<button  style="border-radius: 4px;" class="btn btn-primary" type="submit" ><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart-plus-fill" viewBox="0 3 14 14">
-  <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM9 5.5V7h1.5a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0V8H6.5a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 1 0z"/></svg>
-    Add to Cart</button>
-  </div>
-</form>
+     
 
      <?php echo nl2br("\n"); ?>   <?php echo "Weight: "; ?>{{ $product['weight'] }}<?php echo nl2br("\n"); ?> <?php echo nl2br("\n"); ?> 
 </div>
@@ -82,10 +68,6 @@
     </div>
   </form>
 @endif
-
-
-
->>>>>>> Stashed changes
 
 </div>
 
