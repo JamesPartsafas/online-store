a9cbb35ca1e5530c0266bc3b2411ce4ccfe9abc2
JamesThePlant <56362880+JamesThePlant@users.noreply.github.com>
2022-02-27 23:11:56 -0500

    Add increment/decrement functionality

--- amaznot/resources/views/pages/cart.blade.php
+++ amaznot/resources/views/pages/cart.blade.php
@@ -66,12 +66,12 @@
                         <b ><span id=${index}>${element.amount}</span> x $${element.price}</b>\t
                         ${element.name}<img style="max-width:80px" src=${element.image}>
                     </li>
-                    <button type='button' class="btn btn-info" style="border-radius:17px;"onclick="decrement1(${index})"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash-square" viewBox="0 0 16 16">
+                    <button type='button' class="btn btn-info" style="border-radius:17px;"onclick="decrementCart(${element.id})"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash-square" viewBox="0 0 16 16">
     <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
                 <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z"/></svg>
                  </button>
 
-<button  type='button' class="btn btn-info" style="margin-right:14px;border-radius:17px;" type='button' class="btn btn-primary" onclick="increment1(${index})" ><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
+<button  type='button' class="btn btn-info" style="margin-right:14px;border-radius:17px;" type='button' class="btn btn-primary" onclick="incrementCart(${element.id})" ><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
 <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
 <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/></svg>  </button>
                     `  
@@ -96,6 +96,36 @@
 
         const moneyFormat = (price) => {
             return (Math.round(price * 100) / 100).toFixed(2)
+        }
+
+        const decrementCart = (id) => {
+            let cart = JSON.parse(localStorage.getItem('cart'))
+
+            cart.map((cartItem) => {
+                if (cartItem.id == id)
+                    cartItem.amount--
+            })
+
+            cart = cart.filter((cartItem) => {
+                return (cartItem.amount > 0)
+            })
+
+            localStorage.setItem('cart', JSON.stringify(cart))
+
+            renderScreen()
+        }
+
+        const incrementCart = (id) => {
+            const cart = JSON.parse(localStorage.getItem('cart'))
+
+            cart.map((cartItem) => {
+                if (cartItem.id == id)
+                    cartItem.amount++
+            })
+
+            localStorage.setItem('cart', JSON.stringify(cart))
+
+            renderScreen()
         }
 
 
