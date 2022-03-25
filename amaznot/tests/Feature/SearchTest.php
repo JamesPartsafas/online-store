9510a7a9640e108b4f46cc3e4406941ed446f27a
Siri2K <kanagalingham.sriram@gmail.com>
2022-03-25 08:57:50 -0400

    Added Construct to Search Test

--- amaznot/tests/Feature/SearchTest.php
+++ amaznot/tests/Feature/SearchTest.php
@@ -10,9 +10,17 @@ class SearchTest extends TestCase
 {
     use \Illuminate\Foundation\Testing\DatabaseMigrations;
 
+    private $query;
+
+    function __construct()
+    {
+        parent:: __construct();
+        $this->query = "testQuery";
+    }
+
     public function test_search_page_returns_successfully()
     {
-        $response = $this->get(route('search', ['query' => "Test"])); // TestQuery
+        $response = $this->get(route('search', ['query' => $this->query])); // TestQuery
 
         $response->assertStatus(200);
     }
